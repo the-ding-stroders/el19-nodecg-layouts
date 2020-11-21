f1cf845a7b59a0ee1ede23f1b71063af5fe9315b
David Thomas <dev@synth3.tk>
2020-11-21 14:53:02 -0500

    fix donation check not actually working lol

--- extension/extra-life.js
+++ extension/extra-life.js
@@ -4,7 +4,6 @@ const nodecg = require('./util/nodecg-api-context').get();
 const TEAM_ID = nodecg.bundleConfig.teamID;
 const EL_UPDATE_INTERVAL = nodecg.bundleConfig.elUpdateInterval;
 
-const settingsRep = nodecg.Replicant('settings').value;
 const totalRep = nodecg.Replicant('total');
 const fundGoalRep = nodecg.Replicant('fundraisingGoal');
 const donationRep = nodecg.Replicant(
@@ -23,18 +22,19 @@ function checkForDonations() {
   // Grab the last 10 donations from Extra Life
   extraLife.getTeamDonations(TEAM_ID, 10).then((donationResults) => {
     const elDonations = donationResults.records;
-    const knownDonations = donationRep.value;
+    const knownDonations = nodecg.readReplicant('donations');
     const newDonationsCheck = JSON.stringify(elDonations) !== JSON.stringify(knownDonations);
 
     if (newDonationsCheck === true) {
+      nodecg.log.debug('New donations found');
       // Get the new donations that aren't known by us yet
-      const newDonations = [elDonations, knownDonations].sort((e, k) => k.length - e.length)
-        .reduce((e, k) => e.filter((o) => !k.some((v) => v.id === o.id)));
+      const newDonations = elDonations.filter(({ donationID: id1 }) => !knownDonations.some(({ donationID: id2 }) => id2 === id1));
 
       // Update our known donations with what Extra Life sent
       donationRep.value = elDonations;
       extraLife.getTeam(TEAM_ID).then((teamInfoResults) => {
         // Update the team's total value and send out notifications
+        nodecg.log.debug('Updating total value replicant');
         totalRep.value = teamInfoResults.sumDonations;
         nodecg.sendMessage('donationAlert', newDonations);
         if (settingsRep.discordAlertsEnabled.value === true) {
@@ -67,6 +67,5 @@ function checkForDonations() {
 
 // Check for new donations on an interval
 setInterval(() => {
-  nodecg.log.debug('Checking for new donations');
   checkForDonations();
 }, EL_UPDATE_INTERVAL * 1000);
